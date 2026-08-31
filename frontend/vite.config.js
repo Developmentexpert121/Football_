b92f68fb90e749e3934568cc62a7a47564c17141
Developmentexpert121 <developmentexpert121@gmail.com>
2026-08-31 15:54:05 +0530

    fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://noted-mere-awesome-broker.trycloudflare.com',
+        target: 'https://conditions-unix-charitable-bra.trycloudflare.com',
         changeOrigin: true,
         secure: false,
         timeout: 600000,
@@ -26,7 +26,7 @@ export default defineConfig({
         }
       },
       '/media': {
-        target: 'https://noted-mere-awesome-broker.trycloudflare.com',
+        target: 'https://conditions-unix-charitable-bra.trycloudflare.com',
         changeOrigin: true,
         secure: false,
         timeout: 600000,
