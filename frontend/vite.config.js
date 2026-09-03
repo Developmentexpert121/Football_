7e058701dfd2ace8c2a474c5ee5352b00a6aa09a
Developmentexpert121 <developmentexpert121@gmail.com>
2026-09-03 16:31:43 +0530

    chore(config): update Cloudflare proxy target to https://identifies-records-dam-farming.trycloudflare.com

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://interpreted-slip-wright-lcd.trycloudflare.com',
+        target: 'https://identifies-records-dam-farming.trycloudflare.com',
         changeOrigin: true,
         secure: false,
         timeout: 600000,
@@ -26,7 +26,7 @@ export default defineConfig({
         }
       },
       '/media': {
-        target: 'https://interpreted-slip-wright-lcd.trycloudflare.com',
+        target: 'https://identifies-records-dam-farming.trycloudflare.com',
         changeOrigin: true,
         secure: false,
         timeout: 600000,
